002949a0848a92339e9c8cf053f522007baabab8
Mohamed Zahlan <mohamedzahlan.056@gmail.com>
2022-10-14 21:28:21 +0200

    Delete constants.dart

--- lib/shared/shared.components/constants.dart
+++
@@ -1,8 +0,0 @@
-//https://newapi.org/
-
-String? token = '';
-
-void printFullText(String text) {
-  final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
-  pattern.allMatches(text).forEach((match) => print(match.group(0)));
-}
